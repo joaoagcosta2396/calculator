ca01287a66f143eb6b88427d95c1fee4b8bf3e46
ImperatorXXI <joaoagcosta@hotmail.com>
2023-02-02 20:15:35 +0000

    Javascript done

--- app.js
+++ app.js
@@ -1,109 +1,149 @@
 let operator = '';
-let store_op = '';
-let currentValue = '';
-let previousValue = '';
-let storedValue = '';
+let currentNum = '';
+let previousNum = '';
 
-document.addEventListener("DOMContentLoaded", function(){
-    let clear = document.querySelector(".clear");
-    let equal = document.querySelector(".equal");
-    let decimal = document.querySelector(".decimal");
-
-    let numbers = document.querySelectorAll(".number");
-    let operators = document.querySelectorAll(".operator");
-
-    let displayResult = document.querySelector("#display");
-    
-
-   
-    numbers.forEach((number) => number.addEventListener('click', function(e){
-        currentValue += e.target.textContent;
-        displayResult.textContent = currentValue;
-        
-      
-        
-    }))
+const previousDisplay = document.querySelector(".previousDisplay");
+const currentDisplay = document.querySelector(".currentDisplay");
 
 
-    operators.forEach((operator) => operator.addEventListener('click', function(e){
-        operator = e.target.textContent;
-
-        // store current Value
-
-        if(storedValue != ''){
-            storedValue = Number(storedValue);
-            currentValue = Number(currentValue);
-            store_op = operator;
-            displayResult.textContent = operate(store_op, storedValue, currentValue).toString();
-        }else{
-            previousValue = currentValue;
-            store_op = operator;
-            currentValue = '';
-        }
-
-    
-        
-    
-    }))
 
 
-    equal.addEventListener('click', function(){
+const operators = document.querySelectorAll(".operator");
+const numbers = document.querySelectorAll(".number");
 
-        
-        previousValue = Number(previousValue);
-        currentValue = Number(currentValue);
-        console.log (previousValue, store_op, currentValue);
-        console.log(operate(store_op,previousValue, currentValue));
-    
-        displayResult.textContent = operate(store_op, previousValue, currentValue).toString();
-        previousValue = operate(store_op,previousValue, currentValue);
-        console.log(previousValue);
-    })
-    
+const equal = document.querySelector(".equal");
+equal.addEventListener("click", () => {
+    if(currentNum != '' && previousNum != ''){
+        operate();
+    }
+});
 
-    clear.addEventListener('click', function(){
-        previousValue = '';
-        currentValue = '';
-        displayResult.textContent = currentValue;
-       
-    })
-    
-    
+
+const decimal = document.querySelector(".decimal");
+decimal.addEventListener('click', () => {
+    addDecimal();
 })
 
+const clear = document.querySelector(".clear");
+clear.addEventListener('click', clearCalculator);
 
 
 
+numbers.forEach((btn) => {
+    btn.addEventListener("click", (e) =>{
+        handleNumber(e.target.textContent)
+    });
+});
 
-// aritmetical operations
-function add(num1, num2){
-    return num1+num2;
-}
 
-function subtract(num1, num2){
-    return num1-num2;
-}
-
-function multiply(num1, num2){
-    return num1*num2;
-}
-
-function divide(num1, num2){
-    if (num2 === 0){
-        return "Infinity";
-    }else{
-        return num1/num2;
+function handleNumber(number){
+    if(previousNum !== '' && currentNum !== '' && operator === ""){
+        previousNum = '';
+        currentDisplay.textContent = currentNum;
     }
+    if(currentNum.length <= 11){
+        currentNum += number;
+        currentDisplay.textContent = currentNum;
+    }
+    
+}
+
+
+operators.forEach((btn) => {
+    btn.addEventListener("click", (e) =>{
+        handleOperator(e.target.textContent)
+    });
+});
+
+
+function handleOperator(op){
+    
+    if(previousNum === ""){
+        previousNum = currentNum;
+        operatorCheck(op)
+    }else{
+        if(currentNum  === ""){
+            operatorCheck(op)
+        }else{
+            operate()
+            operator = op;
+            currentDisplay.textContent = "0";
+            previousDisplay.textContent = previousNum + " " +  operator;
+        }
+    }
+    
+}
+
+function operatorCheck(text){
+    operator = text;
+    previousDisplay.textContent = previousNum + " " +  operator;
+    currentDisplay.textContent = '0';
+    currentNum = "";
 
 }
-// function to call operations
-function operate(op, num1, num2){
 
-    switch(op){
-        case "+": return add(num1, num2); 
-        case "-": return subtract(num1, num2); 
-        case "x":  return multiply(num1, num2);
-        case "/":  return divide(num1, num2); 
-        default: return 0;
+
+
+// function to call operations
+function operate(){
+    previousNum = Number(previousNum);
+    currentNum = Number(currentNum);
+
+    if(operator === "+"){
+        previousNum +=  currentNum;
+    }else{
+        if(operator === "-"){
+            previousNum -=  currentNum;
+        }else{
+            if(operator === "x"){
+                previousNum *= currentNum;
+            }else{
+                if(operator === "/"){
+                    if(currentNum<=0){
+                        previousNum = "Error"
+                        displayResult();
+                        return;
+                    }
+                    previousNum /= currentNum;
+                }
+            }
+        }
+    }
+    previousNum = roundNumb(previousNum);
+    previousNum = previousNum.toString();
+    displayResult();
+
+}
+
+function roundNumb(num){
+    return Math.round(num * 100000)/100000;
+}
+
+function displayResult(){
+    
+    
+    if(previousNum.length <= 11){
+        currentDisplay.textContent = previousNum;
+    }else{
+        currentDisplay.textContent = previousNum.slice(0,11) + "...";
+    }
+    previousDisplay.textContent = '';
+    operator = '';
+    currentNum = '';
+}
+
+function clearCalculator(){
+    currentNum = "";
+    previousNum = "";
+    operator = "";
+
+    currentDisplay.textContent = "0";
+    previousDisplay.textContent = "";
+}
+
+function addDecimal(){
+    if(!currentNum.includes('.')){
+        currentNum += ".";
+        currentDisplay.textContent = currentNum;
     }
 }
